6378957390755dcd32628da430517fbabfbb943f
Zhuzhgin <artemfox@list.ru>
2021-12-08 23:48:33 +0500

    Прописал логику

--- svetofor/ViewController.swift
+++ svetofor/ViewController.swift
@@ -8,43 +8,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var redLightView: UIView!
     
+    @IBOutlet var redView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var buttonText: UIButton!
+
     var numberPress = 0
     
     @IBAction func buttonPress(_ sender: Any) {
         
+        buttonText.setTitle("Next", for: .normal)
+        
         numberPress += 1
+        
         switch numberPress {
         case 1 :
-            redLightView.alpha = 1
+            redView.alpha = 1
             yellowView.alpha = 0.3
             greenView.alpha = 0.3
         case 2:
             yellowView.alpha = 1
-            redLightView.alpha = 0.3
+            redView.alpha = 0.3
             greenView.alpha = 0.3
         default :
             greenView.alpha = 1
-            redLightView.alpha = 0.3
+            redView.alpha = 0.3
             yellowView.alpha = 0.3
             numberPress = 0
         }
         
     }
-    @IBOutlet var buttonText: UIButton!
     
-    @IBOutlet var greenView: UIView!
-    @IBOutlet var yellowView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLightView.alpha = 0.3
+        redView.alpha = 0.3
         greenView.alpha = 0.3
         yellowView.alpha = 0.3
-        
-        
-        // Do any additional setup after loading the view.
+       
+        greenView.layer.cornerRadius = 50
+        yellowView.layer.cornerRadius = 50
+        redView.layer.cornerRadius = 50
     }
 
 
