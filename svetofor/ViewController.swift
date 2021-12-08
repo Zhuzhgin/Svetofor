48feca6f57a3ec2bf22b1952d85462d5ec67514e
Zhuzhgin <artemfox@list.ru>
2021-12-08 20:31:49 +0500

    Initial Commit

---
+++ svetofor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  svetofor
+//
+//  Created by Artem Zhuzhgin on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
