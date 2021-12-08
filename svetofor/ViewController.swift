5374d095939a20475d6f04908a746719c0eb8937
Zhuzhgin <artemfox@list.ru>
2021-12-08 21:16:48 +0500

    Задание №2

--- svetofor/ViewController.swift
+++ svetofor/ViewController.swift
@@ -8,9 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redLightView: UIView!
+    var numberPress = 0
+    
+    @IBAction func buttonPress(_ sender: Any) {
+        
+        numberPress += 1
+        switch numberPress {
+        case 1 :
+            redLightView.alpha = 1
+            yellowView.alpha = 0.3
+            greenView.alpha = 0.3
+        case 2:
+            yellowView.alpha = 1
+            redLightView.alpha = 0.3
+            greenView.alpha = 0.3
+        default :
+            greenView.alpha = 1
+            redLightView.alpha = 0.3
+            yellowView.alpha = 0.3
+            numberPress = 0
+        }
+        
+    }
+    @IBOutlet var buttonText: UIButton!
+    
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var yellowView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redLightView.alpha = 0.3
+        greenView.alpha = 0.3
+        yellowView.alpha = 0.3
+        
+        
         // Do any additional setup after loading the view.
     }
 
